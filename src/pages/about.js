3a67509ca93c7ae7c5a12c84e614c39b2f8d1817
Chris Jordan <chris.jordan@news.co.uk>
2018-08-08 09:25:49 +0100

    Adds about me section.

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,41 +6,50 @@ export default class About extends React.Component {
       <div>
         <h2>Introduction</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquid autem dolorum, ducimus enim est ex, exercitationem facere
-          illum in laudantium minus, molestias neque praesentium quaerat quas
-          rerum suscipit totam?
+          I have been developing software professionally since 2011, and have
+          been fortunate to have worked with technologies across the stack -
+          from front-end, back-end to infrastructure. Having a broad range of
+          skills and experience allows me to come up with optimal, pragmatic
+          solutions.
         </p>
+        <p>
+          I put a huge emphasis on building highly maintainable software which
+          can adapt to changing requirements. Closely following best practices
+          in software development has allowed me to achieve this.
+        </p>
+        <p>Here are some of the skills that I've picked up over the years;</p>
         <h2>Front-end</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquid autem dolorum, ducimus enim est ex, exercitationem facere
-          illum in laudantium minus, molestias neque praesentium quaerat quas
-          rerum suscipit totam?
-        </p>
+        <li>React and Redux</li>
+        <li>Angular</li>
+        <li>ES8</li>
+        <li>Electron</li>
+        <li>HTML5/CSS3</li>
+        <li>SCSS/CSS in JS</li>
+
         <h2>Back-end</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquid autem dolorum, ducimus enim est ex, exercitationem facere
-          illum in laudantium minus, molestias neque praesentium quaerat quas
-          rerum suscipit totam?
-        </p>
+        <li>Node/Express</li>
+        <li>Java</li>
+        <li>Ruby on Rails</li>
+        <li>Clojure/Elixir</li>
+        <li>SQL</li>
+
         <h2>Devops/Infrastructure</h2>
-        <p>Thanks for visiting my website</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquid autem dolorum, ducimus enim est ex, exercitationem facere
-          illum in laudantium minus, molestias neque praesentium quaerat quas
-          rerum suscipit totam?
-        </p>
+        <li>Docker/Docker Compose</li>
+        <li>AWS</li>
+        <li>Terraform</li>
+        <li>CloudFormation</li>
+        <li>Serverless</li>
+        <li>NewRelic</li>
+        <li>TeamCity/Jenkins</li>
+
         <h2>Craftsmanship</h2>
-        <p>Thanks for visiting my website</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquid autem dolorum, ducimus enim est ex, exercitationem facere
-          illum in laudantium minus, molestias neque praesentium quaerat quas
-          rerum suscipit totam?
-        </p>
+        <li>TDD</li>
+        <li>100% Unit Test Coverage</li>
+        <li>Integration Testing</li>
+        <li>UI Testing</li>
+        <li>Clean Code</li>
+        <li>Layered Architecture</li>
+        <li>Continuous Integration and deployment</li>
       </div>
     );
   }
